0c0d4a26530de9058ffc24766d69dec7785e2815
Diego Sanz <digosanf@gmail.com>
2025-08-19 10:41:47 +0200

    FIX: Pass GPUScene as copy.

--- src/cuda.cu
+++ src/cuda.cu
@@ -311,7 +311,7 @@ __global__ void kernel(cuda::std::complex<double> *out_complex_pixels, unsigned 
 #endif // #if ENABLE_COLOR_CGH #else
 }
 
-__global__ void test_kernel(const GPUScene &scene, unsigned char *out_pixels, const PointCloudPoint<> *point_cloud, const unsigned int pc_size, const float* phases) {
+__global__ void test_kernel(const GPUScene scene, unsigned char *out_pixels, const PointCloudPoint<> *point_cloud, const unsigned int pc_size, const float* phases) {
     const uint x = threadIdx.x + blockIdx.x * blockDim.x;
     const uint y = threadIdx.y + blockIdx.y * blockDim.y;
     if ((x >= IMAGE_WIDTH) || (y >= IMAGE_HEIGHT)) return;
@@ -370,6 +370,13 @@ __global__ void test_kernel(const GPUScene &scene, unsigned char *out_pixels, co
 
 __host__ void use_cuda_occ(const Scene &scene, unsigned char pixels[], const PointCloud &point_cloud) {
     static constexpr uint num_pixels = IMAGE_WIDTH * IMAGE_HEIGHT;
+
+    printf("         Using: CUDA Occlusion\n");
+    printf("         Image size: %s x %s (factor %d)\n", add_thousand_separator(IMAGE_WIDTH).c_str(),
+           add_thousand_separator(IMAGE_HEIGHT).c_str(), VIRTUAL_SLM_FACTOR);
+    printf("         Num points: %s\n", add_thousand_separator(point_cloud.size()).c_str());
+    printf("         Enable color: %s\n", ENABLE_COLOR_CGH ? "true" : "false");
+    printf("         Image number: %d\n",  NUM_IMAGES);
 
     dim3 block(32, 32);
     dim3 grid(IMAGE_WIDTH / block.x + 1, IMAGE_HEIGHT / block.y + 1);
